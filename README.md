d8e9a8170ece8c7616bb233c3b78c2b6aff86bba
Filip Skoczeń <philipskoczen@gmail.com>
2022-05-05 12:48:22 +0200

    Add unocss

---
+++ README.md
@@ -0,0 +1,4 @@
+# Features
+
+- vite + pnpm for speed
+- UnoCSS for styling
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import css from 'unocss/vite'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  resolve: {
+    alias: {
+      '~/': `${path.join(__dirname, './src')}/`
+    }
+  },
+  plugins: [
+    react(),
+    css(),
+  ]
 })
